2add152874d59df7594d978f0875e3f7f5d1581e
bidy25 <sarobidy@kasia.mg>
2022-07-05 08:51:51 +0300

    backupkd

--- kentia-domiciliation/kentia_domiciliation/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_partner
-from . import product_template
-from . import sale_order
-from . import account_move_inherit
-from . import account_declaration
--- kentia-domiciliation/kentia_domiciliation/models/product_template.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
-
-
-class ProductTemplateInherit(models.Model):
-    _inherit = 'product.template'
-
-    is_creation = fields.Boolean(string='Is creation type')
